777df14309a42d0bc3cf7189163be691aecf620d
Dominic <dominichenryscott@gmail.com>
2024-05-15 14:02:15 -0400

    Added Code for Camera Movement with Player

--- CharacterController-ProjectFiles/Assets/_app/Scripts/Player/CharacterController.cs
+++ CharacterController-ProjectFiles/Assets/_app/Scripts/Player/CharacterController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class CharacterController : MonoBehaviour
 {
+    /*
     [Header("Movement Components")]
     public float movementSpeed;
     public Vector3 jumpforce;
@@ -35,22 +36,40 @@ public class CharacterController : MonoBehaviour
                 GameManager.instance.IncreaseScore();
             }
         }
-
-        if(Input.GetKey(KeyCode.W))
+        // Key Down Events for starting movement
+        if(Input.GetKeyDown(KeyCode.W))
         {
-            movementVector.z = 1;
+            movementVector.y = 1;
         }
-        if(Input.GetKey(KeyCode.S))
+        if(Input.GetKeyDown(KeyCode.S))
         {
-            movementVector.z = -1;
+            movementVector.y = -1;
         }
-        if(Input.GetKey(KeyCode.A))
+        if(Input.GetKeyDown(KeyCode.A))
         {
             movementVector.x = -1;
         }
-        if(Input.GetKey(KeyCode.D))
+        if(Input.GetKeyDown(KeyCode.D))
         {
             movementVector.x = 1;
+        }
+        
+        // Key Up Events for finishing movement
+        if(Input.GetKeyUp(KeyCode.W) && (movementVector.y == 1))
+        {
+            movementVector.y = 0;
+        }
+        if(Input.GetKeyUp(KeyCode.S) && (movementVector.y == -1))
+        {
+            movementVector.y = 0;
+        }
+        if(Input.GetKeyUp(KeyCode.A) && (movementVector.x == -1))
+        {
+            movementVector.x = 0;
+        }
+        if(Input.GetKeyUp(KeyCode.D) && (movementVector.x == 1))
+        {
+            movementVector.x = 0;
         }
 
         PlayerMovement(movementVector);
@@ -64,5 +83,121 @@ public class CharacterController : MonoBehaviour
     private void PlayerMovement(Vector2 movement)
     {
         transform.Translate(translation:new Vector3(movement.x, y:0, movement.y) * movementSpeed * Time.deltaTime);
+    }
+    */
+
+    [Header("Movement")] 
+    public float moveSpeed;
+
+    public float groundDrag;
+    public float jumpForce;
+    public float jumpCooldown;
+    public float airMultiplier;
+    private bool readyToJump;
+
+    [Header("Keybinds")] public KeyCode jumpKey = KeyCode.Space;
+    
+    [Header("Ground Check")] 
+    public Transform groundCheck;
+    public  float groundDistance = 0.4f;
+    public LayerMask whatIsGround;
+    private bool grounded;
+
+    public Transform orientation;
+
+    private float horizontalInput;
+    private float verticalInput;
+
+    private Vector3 moveDirection;
+
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+        readyToJump = true;
+    }
+
+    private void Update()
+    {
+        //  ground check
+        //grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
+        grounded = Physics.CheckSphere(groundCheck.position, groundDistance, whatIsGround); 
+        
+        MyInput();
+        SpeedControl();
+        
+        // handle drag
+        if (grounded)
+        {
+            rb.linearDamping = groundDrag;
+        }
+        else
+        {
+            rb.linearDamping = 0;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    private void MyInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+        
+        // when to jump
+        if (Input.GetKey(jumpKey) && readyToJump && grounded)
+        {
+            readyToJump = false;
+            
+            Jump();
+            
+            Invoke(nameof(ResetJump), jumpCooldown);
+        }
+    }
+
+    private void MovePlayer()
+    {
+        // calculate movement direction
+        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+        
+        // on ground
+        if (grounded)
+        {
+            rb.AddForce(moveDirection.normalized * (moveSpeed * 10f), ForceMode.Force);
+        }
+        else if (!grounded)
+        {
+            rb.AddForce(moveDirection.normalized * (moveSpeed * 10f * airMultiplier), ForceMode.Force);
+        }
+    }
+
+    private void SpeedControl()
+    {
+        Vector3 flatVel = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
+        
+        // limit velocity if needed
+        if (flatVel.magnitude > moveSpeed)
+        {
+            Vector3 limitedVel = flatVel.normalized * moveSpeed;
+            rb.linearVelocity = new Vector3(limitedVel.x, rb.linearVelocity.y, limitedVel.z);
+        }
+    }
+
+    private void Jump()
+    {
+        // reset y velocity
+        rb.linearVelocity = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
+        
+        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+    }
+
+    private void ResetJump()
+    {
+        readyToJump = true;
     }
 }
